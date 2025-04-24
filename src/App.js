ab65847943d6e00dca94e4159e982a300aa0aabc
Divyanshi Gupta <71611825+realdivs@users.noreply.github.com>
2025-04-25 01:20:23 +0530

    Added debouncing and caching

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ export default function App() {
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
   const [showResults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
 
   const fetchData = async () => {
+    if (cache[input]) {
+      setResults(cache[input]);
+      return;
+    }
     const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
     const json = await data.json();
     setResults(json?.recipes);
+    setCache((prev) => ({ ...prev, [input]: json?.recipes }));
   };
 
   useEffect(() => {
-    fetchData();
+    const timer = setTimeout(fetchData, 300);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [input]);
 
   return (
